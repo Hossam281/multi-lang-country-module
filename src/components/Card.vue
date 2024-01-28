661c684aa9d725c810319fe19dd400eb2a482733
Hossam281 <mohamedhosam281@outlook.com>
2024-01-28 19:45:16 +0200

    Updated

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-full p-3 overflow-clip  flex h-[400px] flex-col gap-4 text-white bg-[#393E46] rounded-lg shadow-xl shadow-[#00adb5] ">
         <h1 class="text-3xl font-semibold text-center">{{ country.name ? country.name : "No name provided" }}</h1>
-        <h4 class="text-xl font-mono">Population: {{ country.population ? country.population : "No population provided" }}</h4>
-        <h4 class="text-xl font-mono">capital: {{ country.capital ? country.capital : "No capital provided" }}</h4>
-        <p class="text-md overflow-clip font-mono h-[70%]">{{ country.details ? country.details : "No details provided" }}</p>
+        <h4 class="text-xl font-mono">{{ $t('Population') }}: {{ country.population ? country.population : "No population provided" }}</h4>
+        <h4 class="text-xl font-mono">{{$t('Capital')}}: {{ country.capital ? country.capital : "No capital provided" }}</h4>
+        <p class="text-md overflow-clip font-mono h-[70%]">{{ country.details ? country.details : $t('NoDetails') }}</p>
         <router-link :to="`/details/${country.id}`" class="w-full bg-[#00adb5]  rounded-lg text-center py-2 font-semibold text-lg hover:opacity-40 transition-opacity duration-300">{{ $t('ViewDetails') }}</router-link>
         <div class="w-full flex items-center justify-evenly ">
             <faIcon @click="handleDelete(country.id)" class="cursor-pointer hover:scale-105 transition-all duration-300" :icon="['fas', 'trash']" />
--- src/views/CountryDetails.vue
+++ src/views/CountryDetails.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-[90%] mx-auto flex gap-7 flex-col mt-4">
         <h1 class="text-3xl font-bold text-center">{{ data.name }}</h1>
-        <h4 class="text-2xl font-sans"><span class="font-bold">Population:</span> <br/> {{ data.population }}</h4>
-        <h4 class="text-2xl font-sans"><span class="font-bold">capital:</span> <br/> {{ data.capital }}</h4>
-        <p class="text-md overflow-cl2ip font-sans h-[70%]"><span class="font-bold text-2xl">Details:</span> <br/> {{ data.details ? data.details : "No details provided" }}</p>
+        <h4 class="text-2xl font-sans"><span class="font-bold">{{$t('Population')}}:</span> <br/> {{ data.population }}</h4>
+        <h4 class="text-2xl font-sans"><span class="font-bold">{{$t('Capital')}}:</span> <br/> {{ data.capital }}</h4>
+        <p class="text-md overflow-cl2ip font-sans h-[70%]"><span class="font-bold text-2xl">{{$t('Details')}}:</span> <br/> {{ data.details ? data.details : $t('NoDetails') }}</p>
 
     </div>
 </template>
